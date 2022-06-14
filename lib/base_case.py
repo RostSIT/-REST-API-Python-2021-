b0fa318a733e277c67a20e35060063591f9a7f40
RostSIT <streltsovrp@gmail.com>
2022-06-14 15:29:59 +0300

    add test_user_get.py and upgrade other tests

--- lib/base_case.py
+++ lib/base_case.py
@@ -1,24 +1,17 @@
 import json.decoder
 from requests import Response
+from datetime import datetime
 
 
 class BaseCase:
     def get_cookie(self, response: Response, cookie_name):
         assert cookie_name in response.cookies, \
             f"Cannot find cookie with name {cookie_name} in the last response"
-        print()
-        print('========================================================')
-        print('cookie')
-        print(response.cookies[cookie_name])
         return response.cookies[cookie_name]
 
     def get_header(self, response: Response, headers_name):
         assert headers_name in response.headers, \
             f"Cannot find headers with the name {headers_name} in the last response"
-        print()
-        print('========================================================')
-        print('headers_name')
-        print(response.headers[headers_name])
         return response.headers[headers_name]
 
     def get_json_value(self, response: Response, name):
@@ -29,9 +22,18 @@ class BaseCase:
                 f"Response is not in JSON format. Response text is '{response.text}'"
 
         assert name in response_as_dict, f"Response JSON does not have key '{name}'"
-        print()
-        print('========================================================')
-        print('json')
-        print(response_as_dict[name])
         return response_as_dict[name]
 
+    def prepare_registration_data(self, email=None):
+        if email is None:
+            base_part = "learnqa"
+            domain = "example.com"
+            random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+            email = f"{base_part}{random_part}@{domain}"
+        return {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': email
+        }
