8b9226746b0eccb92aebead6f16cfc4da96bb9df
RostSIT <streltsovrp@gmail.com>
2022-06-14 20:19:44 +0300

    update test_user_get.py, test_user_auth.py, test_user_edit.py, test_user_register.py (implement MyRequests methods)

--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -1,4 +1,4 @@
-import requests
+from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 
@@ -7,7 +7,7 @@ class TestUserEdit(BaseCase):
     def test_edit_just_created_user(self):
         # REGISTER
         register_data = self.prepare_registration_data()
-        response1 = requests.post("https://playground.learnqa.ru/api/user/", data=register_data)
+        response1 = MyRequests.post("/user/", data=register_data)
 
         Assertions.assert_code_status(response1, 200)
         Assertions.assert_json_has_key(response1, "id")
@@ -16,13 +16,12 @@ class TestUserEdit(BaseCase):
         first_name = register_data['firstName']
         password = register_data['password']
         user_id = self.get_json_value(response1, "id")
-        print(first_name)
         #  LOGIN
         login_data = {
             'email': email,
             'password': password
         }
-        response2 = requests.post("https://playground.learnqa.ru/api/user/login", data=login_data)
+        response2 = MyRequests.post("/user/login", data=login_data)
 
         auth_sid = self.get_cookie(response2, "auth_sid")
         token = self.get_header(response2, "x-csrf-token")
@@ -30,21 +29,18 @@ class TestUserEdit(BaseCase):
         # EDIT
         new_name = "Changed Name"
 
-
-        response3 = requests.put(
-            f"https://playground.learnqa.ru/api/user/{user_id}",
+        response3 = MyRequests.put(
+            f"/user/{user_id}",
             headers={"x-csrf-token": token},
             cookies={"auth_sid": auth_sid},
             data={"firstName": new_name}
         )
 
-
         Assertions.assert_code_status(response3, 200)
 
-
-        #GET
-        response4 = requests.get(
-            f"https://playground.learnqa.ru/api/user/{user_id}",
+        # GET
+        response4 = MyRequests.get(
+            f"/user/{user_id}",
             headers={"x-csrf-token": token},
             cookies={"auth_sid": auth_sid}
         )
@@ -55,4 +51,3 @@ class TestUserEdit(BaseCase):
             new_name,
             "Wrong name of the user after edit"
         )
-        print(new_name)
